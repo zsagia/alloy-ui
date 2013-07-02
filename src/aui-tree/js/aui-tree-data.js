98050c404d62178716fd2636cd04265e64d11c00
Tibor Lipusz <tibor.lipusz@liferay.com>
2013-07-02 12:03:48 +0200

    AUI-812 Update childrenLength when setting children

--- src/aui-tree/js/aui-tree-data.js
+++ src/aui-tree/js/aui-tree-data.js
@@ -773,6 +773,22 @@ A.mix(TreeData.prototype, {
 	_setChildren: function(v) {
 		var instance = this;
 		var childNodes = [];
+		var container = instance.get(CONTAINER);
+
+		if (!container) {
+			container = instance._createNodeContainer();
+		}
+
+		instance.childrenLength = v.length;
+
+		// before render the node, make sure the PARENT_NODE and OWNER_TREE references are updated
+		// this is required on the render phase of the TreeNode (_createNodeContainer)
+		// to propagate the events callback (appendChild/expand)
+		var ownerTree = instance;
+
+		if (isTreeNode(instance)) {
+			ownerTree = instance.get(OWNER_TREE);
+		}
 
 		var hasOwnerTree = isTreeView(ownerTree);
 		var lazyLoad = true;
@@ -783,7 +799,7 @@ A.mix(TreeData.prototype, {
 
 		instance.updateIndex({});
 
-		if (v.length > 0) {
+		if (instance.childrenLength > 0) {
 			instance.set(LEAF, false);
 		}
 
@@ -806,8 +822,6 @@ A.mix(TreeData.prototype, {
 					node = instance.createNode(node);
 
 					if (hasChildren && lazyLoad) {
-						node.childrenLength = children.length;
-
 						A.setTimeout(function() {
 							node.set(CHILDREN, children);
 						}, 50);
