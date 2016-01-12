18e2b377a59af804dab7376c396b3034b646ca10
Peter Borkuti <peter.borkuti@liferay.com>
2016-01-12 14:22:00 +0100

    AUI-2038 Don't save options when Edit Container is not in use.

--- src/aui-datatable/js/aui-datatable-edit.js
+++ src/aui-datatable/js/aui-datatable-edit.js
@@ -1451,9 +1451,9 @@ var BaseOptionsCellEditor = A.Component.create({
             var instance = this;
             var editContainer = instance.editContainer;
 
-            if (editContainer) {
-                var names = editContainer.all(_DOT + CSS_CELLEDITOR_EDIT_INPUT_NAME);
-                var values = editContainer.all(_DOT + CSS_CELLEDITOR_EDIT_INPUT_VALUE);
+            if (editContainer && !editContainer.hasAttribute('hidden')) {
+                var names = editContainer.all('.' + CSS_CELLEDITOR_EDIT_INPUT_NAME);
+                var values = editContainer.all('.' + CSS_CELLEDITOR_EDIT_INPUT_VALUE);
                 var options = {};
 
                 names.each(function(inputName, index) {
